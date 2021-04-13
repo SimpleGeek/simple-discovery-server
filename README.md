99daf0cbc1a787301ced1ed217953dd71ebe4e98
SimpleGeek <isaiah@livebythecode.us>
2021-04-12 20:29:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Discovery Server
+
+A very simple Spring Boot and Eureka based discovery server.  By default,
+this application will run on port 8761.
---
+++ src/test/java/com/simplegeek/eureka/SimpleDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplegeek.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
